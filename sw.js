c60729e0a870c249ebb0e98bbd540d987634c332
kiyono@somes <kiyono@somes.co.jp>
2026-09-11 11:39:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'menseki-v1';
+const CACHE = 'menseki-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -21,7 +21,18 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  /* Network first for HTML, cache-first for assets */
+  const url = new URL(e.request.url);
+  const isHtml = url.pathname.endsWith('.html') || url.pathname.endsWith('/');
+  if(isHtml){
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+  }
 });
